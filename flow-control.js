ec6b8ea01aee5f88498296a5a5bf55790965cdd5
Majed Amin <majedamin974@gmail.com>
2019-05-28 13:22:21 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,17 +1,39 @@
 function basicTeenager(age) {
+  if (age > 12 && age < 20) {
+    return "You are a teenager!";
+  }
 
+  else {
+    return undefined;
+  }
 }
 
 function teenager(age) {
+  if (age > 12 && age < 20) {
+    return "You are a teenager!";
+  }
 
+  else {
+    return "You are not a teenager";
+  }
 }
 
 function ageChecker(age) {
+  if (age > 12 && age < 20) {
+    return "You are a teenager!";
+  }
 
+  else if (age <= 12) {
+    return "You are a kid";
+  }
+
+  else if (age >= 20) {
+    return "You are a grownup";
+  }
 }
 
 function ternaryTeenager(age) {
-
+  // (age > 12 && age < 20) ? 'You are a teenager' : 'You are not a teenager';
 }
 
 function switchAge(age) {
