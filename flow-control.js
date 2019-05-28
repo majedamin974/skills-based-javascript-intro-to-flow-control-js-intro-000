207921b0a6c69a3e558bb80d36402fbcb6d78fea
Majed Amin <majedamin974@gmail.com>
2019-05-28 13:46:14 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -38,12 +38,17 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch (age) {
-    case (age > 12 && age < 20):
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
       return "You are a teenager";
       break;
-    case (!(age > 12 && age < 20)):
-      return "You are not a teenager";
-      break;
+      
     default:
       return "You have an age";
   }
